9c4f0947aaf9da345096ec047972be9a1a255d24
eclipse <1083033970@qq.com>
2019-04-22 16:35:20 +0800

    ghf

---
+++ src/main/java/Tesa.java
@@ -0,0 +1,34 @@
+/**   
+* @Title: Tesa.java 
+* @Package  
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2019年4月22日 下午4:13:31 
+* @version V1.0   
+*/
+
+/** 
+* @ClassName: Tesa 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author hyq
+* @date 2019年4月22日 下午4:13:31 
+*  
+*/
+public class Tesa {
+private int a;
+
+/**
+ * @return the a
+ */
+public int getA() {
+	return a;
+}
+
+/**
+ * @param a the a to set
+ */
+public void setA(int a) {
+	this.a = a;
+}
+
+}
